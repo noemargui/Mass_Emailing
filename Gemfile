0f699df285f789da8f5e58ee2899b0a42bc6a3f4
Noé Marguillard <noe.marguillard@essec.edu>
2019-01-25 16:23:54 +0100

    changements

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 ruby '2.5.1'
 gem 'nokogiri'
 gem 'json'
-gem 'google_drive'
-gem 'csv'
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -2,27 +2,21 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'json'
-require 'google_drive'
-require 'csv'
 
 class Scrapper
 
-# --------------------------------
-#  Ci-dessous les 4 méthodes me permettant de faire mon scrapping de mail proprement
-# --------------------------------
-
-	def get_townhall_names # ici on récupère les noms
+	def get_townhall_names(department) # ici on récupère les noms
 		townhall_names = []
-		page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+		page = Nokogiri::HTML(open(department))
 		page.xpath("//p/a").each do |name|
 			townhall_names << name.text
 		end
 		return townhall_names
 	end
 
-	def get_townhall_urls # ici on récupère les urls
+	def get_townhall_urls(department) # ici on récupère les urls
 		townhall_url = []
-		page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+		page = Nokogiri::HTML(open(department))
 		page.xpath("//p/a/@href").each do |hall|
 			townhall_url << "http://annuaire-des-mairies.com" + hall.to_str[1..-1]
 		end
@@ -40,67 +34,47 @@ class Scrapper
 	 return townhall_email
 	end
 
-	def make_the_hash(townhall_names, townhall_url, townhall_email) # ici on crée le hash final contenant toutes les informations
-		hash_final = Hash[get_townhall_names.zip(get_townhall_email(get_townhall_urls))]
+	def get_townhall_department(townhall_url) # ici on récupère les emails, grâce aux urls en placés en paramètres
+	 townhall_department = []
+	 townhall_url.each do |url_town|
+	   page = Nokogiri::HTML(open(url_town))
+	   page.xpath("'//main/section[1]/div/div/div/p[1]/a'").each do |department|
+	     townhall_department << department.text
+	   end
+	 end
+	 return townhall_department
+	end
+
+	def make_the_hash(townhall_names, townhall_url, townhall_email, townhall_department) # ici on crée le hash final contenant toutes les informations
+		hash_final = Hash[townhall_names.zip(townhall_email, townhall_department)]
 	  return hash_final
 	end
 
-# --------------------------------
-#  Méthode qui permet d'exporter les données vers un fichier JSON
-# --------------------------------
-
 	def save_as_json(hash_final)
-		File.open("/Users/noemargui/Desktop/THP/Week3/Day12/Mass_Data_Saving/db/emails.json","w") do |f|
+		File.open("db/emails.json","w") do |f|
 			f.write(hash_final.to_json)
 		end
 	end
 
-# --------------------------------
-#  Méthode qui permet d'exporter les données vers mon fichier SpreadSheet : https://docs.google.com/spreadsheets/d/1WdQLc0yBbIqcyjyQGj3ZPAChYY5KHjltJDxogtcWgr8/edit#gid=0
-# --------------------------------
-
-	def save_as_spreadsheet(townhall_names, townhall_email)
-		session = GoogleDrive::Session.from_config("config.json")
-		ws = session.spreadsheet_by_key("1WdQLc0yBbIqcyjyQGj3ZPAChYY5KHjltJDxogtcWgr8").worksheets[0]
-		ws[1, 1] = "Noms des Villes"
-		i = 2
-		y = 0
-		while i < 186
-			ws[i, 1] = townhall_names[y]
-			i = i+1
-			y = y+1
-		end
-		ws[1, 2] = "E-mails des dites mairie"
-		i = 2
-		y = 0
-		while i < 186
-			ws[i, 2] = townhall_email[y]
-			i = i+1
-			y = y+1
-		end
-		ws.save
-	end
-
-# --------------------------------
-#  Méthode qui permet d'exporter les données vers un fichier CSV
-# --------------------------------
-
-	def save_as_csv(hash_final)
-		CSV.open("/Users/noemargui/Desktop/THP/Week3/Day12/Mass_Data_Saving/db/emails.csv", "wb") {|csv| hash_final.to_a.each {|elem| csv << elem} }
-	end
-
-# --------------------------------
-#  Mon perform qui permet de faire tourner toute la machine
-# --------------------------------
-
 	def perform
-		townhall_names = get_townhall_names
-		townhall_url = get_townhall_urls
+		#departments = ["http://annuaire-des-mairies.com/lot-et-garonne.html", "http://annuaire-des-mairies.com/alpes-maritimes.html", "http://annuaire-des-mairies.com/hautes-alpes.html"]
+		department = "http://annuaire-des-mairies.com/lot-et-garonne.html"
+		townhall_names = get_townhall_names(department)
+		townhall_url = get_townhall_urls(department)
 		townhall_email = get_townhall_email(townhall_url)
-		hash_final = make_the_hash(townhall_names, townhall_url, townhall_email)
+		townhall_department = get_townhall_department(townhall_url)
+		department = "http://annuaire-des-mairies.com/alpes-maritimes.html"
+		townhall_names = get_townhall_names(department)
+		townhall_url = get_townhall_urls(department)
+		townhall_email = get_townhall_email(townhall_url)
+		townhall_department = get_townhall_department(townhall_url)
+		department = "http://annuaire-des-mairies.com/hautes-alpes.html"
+		townhall_names = get_townhall_names(department)
+		townhall_url = get_townhall_urls(department)
+		townhall_email = get_townhall_email(townhall_url)
+		townhall_department = get_townhall_department(townhall_url)
+		hash_final = make_the_hash(townhall_names, townhall_url, townhall_email, townhall_department)
 		save_as_json(hash_final)
-		save_as_spreadsheet(townhall_names, townhall_email)
-		save_as_csv(hash_final)
 	end
 
 end
